56ac45950f71ec19e5dee4d8934689e9e92ca8e9
wuzedewuze <wuyang_229@126.com>
2017-07-10 10:42:52 +0800

    tar_files 增加了去除大小为0的文件

--- 备份脚本/tar_files.sh
+++ 备份脚本/tar_files.sh
@@ -7,8 +7,7 @@ export LANG=C
 
 log_path=/var/project_log
 
-find $log_path  -type f -mtime +7 ! -name "*.tar.gz"|xargs  -I {}  tar czvfP {}.tar.gz {} --remove-files 
-
+find $log_path  -type f -mtime +1 -size +0c ! -name "*.tar.gz"|xargs  -I {}  tar czvfP {}.tar.gz {} --remove-files 
 
 exit 0;
 
